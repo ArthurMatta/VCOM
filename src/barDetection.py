0b50383ece7e5a0749ba2e2f549e0cc8a9f7ec0b
arthurmatta <arthurmatta@hotmail.com>
2019-10-28 17:21:56 +0000

    Applied changes made by Francisco

--- src/barDetection.py
+++ src/barDetection.py
@@ -181,6 +181,179 @@ def stretch(img, coordinates):
     return warp
 
 
+# https://stackoverflow.com/questions/56905592/automatic-contrast-and-brightness-adjustment-of-a-color-photo-of-a-sheet-of-pape
+def adjust_contrast_brightness(image, histPercent=1):
+    # Convert to Grayscale
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    # Calculate grayscale histogram
+    hist = cv.calcHist([gray], [0], None, [256], [0, 256])
+    histSize = len(hist)
+
+    # Calculate cumulative distribution from the histogram
+    accumulator = []
+    accumulator.append(float(hist[0]))
+    for index in range(1, histSize):
+        accumulator.append(accumulator[index - 1] + float(hist[index]))
+
+    # Locate points to clip
+    maximum = accumulator[-1]
+    histPercent *= (maximum / 100.0)
+    histPercent /= 2.0
+
+    # Locate Left Cut
+    minGray = 0
+    while accumulator[minGray] < histPercent:
+        minGray += 1
+
+    # Locate Right Cut
+    maxGray = histSize - 1
+    while accumulator[maxGray] >= (maximum - histPercent):
+        maxGray -= 1
+
+    # Calculate Alpha and Beta values
+    # g(i,j) = alpha * f(i,j) + beta
+    # alpha = 255 / (maxGray - minGray)
+    # beta = -minGray * alpha
+    alpha = 255 / (maxGray - minGray)
+    beta = -minGray * alpha
+
+    return cv.convertScaleAbs(image, alpha=alpha, beta=beta)
+
+
+def dotted(img):
+    # Define Dotted Colors
+    red = [0, 0, 255]
+    blue = [255, 0, 0]
+
+    # Get Shape
+    height, width, channels = img.shape
+
+    # Get Middle Height
+    middleHeight = int(height / 2)
+
+    # Get 1/6 Height
+    sixHeight = int(height / 6)
+
+    # Auto Adjust Brightness and Contrast
+    img = adjust_contrast_brightness(img)
+
+    # Plot Dotted Line for Barcode
+    for x in range(width):
+
+        # Get Pixel
+        pixel = img[middleHeight, x]
+
+        # Gray Value
+        gray = 0.02126 * pixel[2] + 0.7152 * pixel[1] + 0.0722 * pixel[0]
+
+        # Decide Result Based On Middle Gray
+        final = blue
+        if gray < 127:
+            final = red
+
+        # Apply Result in 2/6th of Height
+        for y in range(sixHeight * 2):
+            img[middleHeight + y - sixHeight, x] = final
+
+    return img
+
+
+# https://cosmosmagazine.com/technology/how-do-barcodes-work
+def describe(img):
+    print('\nStarting Barcode Description\n')
+
+    # Get Shape
+    height, width, channels = img.shape
+
+    # Get Middle Height
+    middleHeight = int(height / 2)
+
+    # Detect Barcode Left Guard
+    lGuard = -1
+    for x in range(width):
+
+        # Get Pixel
+        pixel = img[middleHeight, x]
+
+        # Found Red(Black Bar)
+        if pixel[2] == 255:
+            lGuard = x
+            break
+
+    print(f'Barcode Left Guard detected at: {lGuard} pixel')
+
+    # Detect Barcode Right Guard
+    rGuard = -1
+    for x in range(width):
+
+        # Get Pixel
+        pixel = img[middleHeight, width - x - 1]
+
+        # Found Red(Black Bar)
+        if pixel[2] == 255:
+            rGuard = width - x - 1
+            break
+
+    print(f'Barcode Right Guard detected at: {rGuard} pixel\n')
+
+    # Calculate Total Size
+    total = rGuard - lGuard
+
+    # Describe Bars/Spaces in Percentages
+    start = -1
+    name = ""
+    for i in range(lGuard, rGuard + 1):
+
+        # Next Pixel
+        pixel = img[middleHeight, i]
+
+        # Detect Sequence Break Space -> Bar
+        if pixel[2] == 255 and name == "Bar":
+            # Calculate Percentage
+            end = i
+            percent = round((end - start) / total * 100, 2)
+            print(f'Space with {percent}%')
+
+            # Restart
+            start = -1
+            name = ""
+
+        # Detect Sequence Break Bar -> Space
+        if pixel[0] == 255 and name == "Space":
+            # Calculate Percentage
+            end = i
+            percent = round((end - start) / total * 100, 2)
+            print(f'Bar with {percent}%')
+
+            # Restart
+            start = -1
+            name = ""
+
+        # Start new measure
+        if start == -1:
+            # Mark start
+            start = i
+
+            # Identify Bar/Space
+            if pixel[2] == 255:
+                name = "Space"
+            else:
+                name = "Bar"
+
+    # If Start was up something was pending
+    if start != -1:
+        # Calculate Percentage
+        end = width
+        percent = round((end - start) / total * 100, 2)
+
+        # Flip type
+        if name == "Space":
+            print(f'Bar with {percent}%')
+        else:
+            print(f'Space with {percent}%')
+
+
 def bar_detection(image):
     # Convert image to grayscale
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -210,6 +383,13 @@ def image_detection(filename):
 
         cv.imshow("Final", final)
         cv.imshow("Warp", warp)
+
+        # Plot Barcode
+        dot_plot = dotted(warp)
+        cv.imshow('Dotted', dot_plot)
+
+        # Describe Barcode
+        describe(dot_plot)
     else:
         cv.imshow("Image", image)
 

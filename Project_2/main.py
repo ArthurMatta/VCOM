a43b5aaaaccd7e7ae7892457811e89e0a20c49ca
arthurmatta <arthurmatta@hotmail.com>
2019-12-18 20:41:55 +0000

    Implemented cube extraction

--- Project_2/main.py
+++ Project_2/main.py
@@ -12,6 +12,7 @@ data = sorted(data)
 masks = [file for file in list(walk('masks'))[0][2] if file.endswith('.mhd')]
 masks = sorted(masks)
 
+# Read nodules csv
 lines = readCsv('trainset_csv/trainNodules.csv')
 header = lines[0]
 nodules = lines[1:]
@@ -40,16 +41,36 @@ for LNDbID, LNDbValues in LNDb.items():
         RadID = Mask[::-1][4]
 
         for nod in LNDbNodules:
-            if nod[0] == LNDbID and nod[1] == RadID:
-                FindingID = nod[2]
+            if nod[header.index('LNDbID')] == LNDbID and nod[header.index('RadID')] == RadID:
+                FindingID = nod[header.index('FindingID')]
                 ctr = np.array(
                     [float(nod[header.index('x')]), float(nod[header.index('y')]), float(nod[header.index('z')])])
 
+                # Convert coordinates to image
                 transfmat_toimg, transfmat_toworld = getImgWorldTransfMats(spacing, transfmat)
                 ctr = convertToImgCoord(ctr, origin, transfmat_toimg)
 
+                # Display nodule scan/mask slice
                 fig, axs = plt.subplots(1, 2)
                 axs[0].imshow(scan[int(ctr[2])])
                 axs[1].imshow(mask[int(ctr[2])])
-                plt.title(f'LNDbID: {LNDbID} | RadID: {RadID} | FindingID: {FindingID}')
+                fig.suptitle(f'LNDbID: {LNDbID} | RadID: {RadID} | FindingID: {FindingID}')
+                plt.show()
+
+                # Extract cube around nodule
+                scan_cube = extractCube(scan, spacing, ctr)
+                mk = mask.copy()
+                mk[mk != int(FindingID)] = 0
+                mk[mk > 0] = 1
+                mask_cube = extractCube(mk, spacing, ctr)
+
+                # Display mid slices from resampled scan/mask
+                fig, axs = plt.subplots(2, 3)
+                axs[0, 0].imshow(scan_cube[int(scan_cube.shape[0] / 2), :, :])
+                axs[1, 0].imshow(mask_cube[int(mask_cube.shape[0] / 2), :, :])
+                axs[0, 1].imshow(scan_cube[:, int(scan_cube.shape[1] / 2), :])
+                axs[1, 1].imshow(mask_cube[:, int(mask_cube.shape[1] / 2), :])
+                axs[0, 2].imshow(scan_cube[:, :, int(scan_cube.shape[2] / 2)])
+                axs[1, 2].imshow(mask_cube[:, :, int(mask_cube.shape[2] / 2)])
+                fig.suptitle(f'LNDbID: {LNDbID} | RadID: {RadID} | FindingID: {FindingID}')
                 plt.show()

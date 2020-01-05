657ce9a9d6526c0373c6cd2e066726dd94379e15
Alexandreaam <alex.al.mar@hotmail.com>
2020-01-05 15:32:47 +0000

    Code cleanup

--- Project_2/CNN.py
+++ Project_2/CNN.py
@@ -16,26 +16,25 @@ Y_test = to_categorical(Y_test)
 # Create CNN model
 nb_filters = 32
 pool_size = (2, 2)
-kernel_size = (4, 4)
+kernel_size = (5, 5)
 input_shape = X[0].shape
 num_classes = 4
 
 model = Sequential()
 
 model.add(Convolution2D(nb_filters, kernel_size, input_shape=input_shape, activation='relu', padding='same'))
-model.add(Convolution2D(nb_filters, kernel_size, activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=pool_size, strides=(2, 2)))
+model.add(MaxPooling2D(pool_size=pool_size))
 
 model.add(Convolution2D(nb_filters*2, kernel_size, activation='relu', padding='same'))
-model.add(Convolution2D(nb_filters*2, kernel_size, activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=pool_size, strides=(2, 2)))
+model.add(MaxPooling2D(pool_size=pool_size))
 
 model.add(Convolution2D(nb_filters*4, kernel_size, activation='relu', padding='same'))
-model.add(Convolution2D(nb_filters*4, kernel_size, activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=pool_size, strides=(2, 2)))
+model.add(MaxPooling2D(pool_size=pool_size))
+
+model.add(Convolution2D(nb_filters*8, kernel_size, activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=pool_size))
 
 model.add(Flatten())
-model.add(Dense(64, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='softmax'))
@@ -43,10 +42,8 @@ model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['ac
 
 model.summary()
 
-# Train the model
 history = model.fit(X_train, Y_train, batch_size=8, epochs=20, verbose=1, validation_split=0.1)
 
-# Test the model
 loss, score = model.evaluate(X_train, Y_train)
 print(f'Test accuracy (train): {score:0.05f} loss: {loss:0.05f}')
 
